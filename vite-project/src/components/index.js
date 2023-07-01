2ce07244ea5615454a699f931f5f97b9615a721d
Angel Tejada <angelfernan018@gmail.com>
2023-07-01 12:35:27 +0200

    First Commit. Structure + navbar

---
+++ vite-project/src/components/index.js
@@ -0,0 +1,24 @@
+//Imports y exports de los components aquí para que el codigo sea mas limpio en App.jsx
+import Navbar from "./Navbar";
+import Billing from "./Billing";
+import CardDeal from "./CardDeal";
+import Business from "./Business";
+import Clients from "./Clients";
+import CTA from "./CTA";
+import Stats from "./Stats";
+import Footer from "./Footer";
+import Testimonials from "./Testimonials";
+import Hero from "./Hero";
+
+export {
+  Navbar,
+  Billing,
+  CardDeal,
+  Business,
+  Clients,
+  CTA,
+  Stats,
+  Footer,
+  Testimonials,
+  Hero,
+};
